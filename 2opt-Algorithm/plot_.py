6d13420d443e96d128085d301a54ab2a7af0c7b5
The-DarK-os <anisri477@gmail.com>
2023-03-03 20:48:00 +0530

    edited 2opt

--- 2opt-Algorithm/plot_.py
+++ 2opt-Algorithm/plot_.py
@@ -9,6 +9,6 @@ new_cities_order = np.concatenate((cities[route],cities[route[0]].reshape(1,2)))
 print(f"Cities: {cities}")
 
 plt.scatter(cities[:,0],cities[:,1])
-plt.plot(new_cities_order[:,0],new_cities_order[:,1])
+plt.plot(new_cities_order[:,0],new_cities_order[:,1],'--',color='red')
 plt.show()
 print(f"Route distance: {path_distance(route,cities):.2f}")
